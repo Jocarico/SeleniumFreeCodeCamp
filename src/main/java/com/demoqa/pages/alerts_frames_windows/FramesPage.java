86b1136ae828a6b43da7e5d9f7412541d32904a2
Jonatan Rico <jonatancalebrico@gmail.com>
2025-07-22 18:02:06 -0600

    5.13.36

---
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -0,0 +1,22 @@
+package com.demoqa.pages.alerts_frames_windows;
+
+import org.openqa.selenium.By;
+
+public class FramesPage extends Alerts_Frames_WindowsPage{
+
+    private By textInFrame = By.id("sampleHeading");
+    private String iFrameBigBox = "frame1";
+    private By headerFramesText = By.xpath("//div[@id='app']//h1[text()='Frames']");
+
+    private void switchToBigBox() {
+        driver.switchTo().frame(iFrameBigBox);
+    }
+
+    public String getTextInBigFrame() {
+        switchToBigBox();
+        String bigFrameText = find(textInFrame).getText();
+        System.out.println("Big Frame Text: " + bigFrameText);
+        return bigFrameText;
+    }
+
+}
